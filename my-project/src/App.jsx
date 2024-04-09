6d1df0e1d077fe4301b778ac4af61f34daf9b3cf
Raihan115219 <aburaihan115219@gmail.com>
2024-04-09 13:23:56 +0600

    design

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,6 +4,24 @@ import "./App.css";
 import ViewInTable from "./comonents/Common/ViewInTable";
 
 function App() {
+  const employee = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    jobId: "123456",
+    jobTitle: "Software Engineer",
+    location: "New York",
+    site: "Headquarters",
+    manager: { firstName: "Jane", lastName: "Smith", staffID: "789012" },
+    category: "Full-time",
+    arcID: "ABC123",
+    altEmail: "john.doe@example.com",
+    streetAddress: "123 Main St",
+    city: "Anytown",
+    cellPhone: "555-555-5555",
+    state: "NY",
+    zipCode: "12345",
+  };
   return (
     <div className="container mx-auto my-5">
       <div className="d-flex justify-center gap-6 ">
@@ -16,7 +34,7 @@ function App() {
       </div>
 
       {/* Table component wil be here */}
-      <ViewInTable />
+      <ViewInTable employee={employee} />
     </div>
   );
 }
--- my-project/src/comonents/Common/ViewInTable.jsx
+++ my-project/src/comonents/Common/ViewInTable.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ViewInTable = () => {
+const ViewInTable = ({ employee }) => {
   return (
     <div className="my-5">
       <div className="overflow-x-auto">
@@ -28,6 +28,24 @@ const ViewInTable = () => {
               <td>Blue</td>
             </tr>
             {/* row 2 */}
+          </tbody>
+        </table>
+      </div>
+      <div className="overflow-hidden">
+        <table className="table-auto">
+          <tbody className="divide-y divide-gray-200">
+            {Object.entries(employee).map(([key, value]) => (
+              <tr key={key} className="bg-white">
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <span className="text-sm font-medium text-gray-900">
+                    {key.replace(/([A-Z])/g, " $1").trim()}
+                  </span>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <span className="text-sm text-gray-500">{value}</span>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>

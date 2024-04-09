9439febab7ccfac705f098224f7c2c89acd787ab
Raihan115219 <aburaihan115219@gmail.com>
2024-04-09 16:25:49 +0600

    working on design

--- my-project/src/comonents/Common/ViewInTable.jsx
+++ my-project/src/comonents/Common/ViewInTable.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
+// eslint-disable-next-line react/prop-types
 const ViewInTable = ({ data, Title }) => {
   const checkValuesOfMine = Object.entries(data).map((item, i) => item);
 
-  const checkValueCorrectOrNot = (value) => {
+  const CorrectObjectValue = (value) => {
     if (typeof value === "object") {
-      return Object.entries(value).map(([subKey, subValue], i) => (
+      return Object.entries(value).map(([secondaryKey, secondaryValue], i) => (
         <div key={i}>
-          <span className="font-medium">{subKey}</span>
-          <span className="ml-1">: {subValue}</span>
+          <span className="font-medium">
+            {secondaryKey
+              .split(/(?=[A-Z])/)
+              .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+              .join(" ")}
+          </span>
+          <span className="ml-1">: {secondaryValue}</span>
         </div>
       ));
     } else {
@@ -19,13 +25,40 @@ const ViewInTable = ({ data, Title }) => {
   return (
     <div>
       <div className="my-5">
+        <div className="overflow-x-auto"></div>
+
         <h1 className="text-3xl font-bold">{Title}</h1>
         <div className="border border-blue-100 p-5">
           <div className="grid grid-cols-2">
             {checkValuesOfMine?.map(([key, value], index) => {
               return (
                 <div key={index}>
-                  <div className="grid grid-flow-col justify-normal">
+                  <table className="table overflow-x-auto table-lg">
+                    <tbody>
+                      <thead>
+                        <tr>
+                          <th>
+                            {key
+                              .split(/(?=[A-Z])/)
+                              .map(
+                                (part) =>
+                                  part.charAt(0).toUpperCase() + part.slice(1)
+                              )
+                              .join(" ")}
+                          </th>
+                          <th>:</th>
+                          <th>{CorrectObjectValue(value)}</th>
+                        </tr>
+                      </thead>
+                    </tbody>
+                  </table>
+                </div>
+              );
+            })}
+            {/* {checkValuesOfMine?.map(([key, value], index) => {
+              return (
+                <div key={index}>
+                  <div className="flex">
                     <div>
                       {key
                         .split(/(?=[A-Z])/)
@@ -39,7 +72,7 @@ const ViewInTable = ({ data, Title }) => {
                   </div>
                 </div>
               );
-            })}
+            })} */}
           </div>
         </div>
       </div>

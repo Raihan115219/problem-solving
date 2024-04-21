8525d53a43586333ce977bfa906056e474732c39
Raihan115219 <aburaihan115219@gmail.com>
2024-04-21 19:01:15 +0600

    added

---
+++ my-project/src/comonents/Common/test.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const test = ({}) => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, hic.</p>
+      <h1>Hello world....</h1>
+
+      <div>
+        <h1>Hello world</h1>
+        <p>Hello world.....</p>
+        <p>
+          amar sonar bangla ami tomai valobashi chirodin tomar akakash, tomar
+          batas amar prane o ma prame bajai basi sonar bangla ami tomai
+          valobashi
+        </p>
+        <p>Hellow, 
+            i am form shibganj, chapai nababganj, will be hieino
+
+
+            tum
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default test;
